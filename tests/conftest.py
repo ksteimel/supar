914c0e73ad63469d6e1730439c80acd7965b5762
ksteimel <ksteimel@iu.edu>
2024-03-10 22:16:30 -0400

    building dataset after initializing it (this was causing errors due to using the wrong transform)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,11 @@
 from pathlib import Path
+from typing import List, Dict
 import pytest
+from supar.models.dep.biaffine.transform import CoNLL
+from supar.utils.field import Field, RawField
+from supar.utils.data import Dataset
+from supar.utils.common import BOS, PAD, UNK
+
 
 @pytest.fixture
 def test_fixtures_path() -> Path:
@@ -17,3 +23,40 @@ def augmented_sample_conllu_file_path(test_fixtures_path: Path) -> Path:
     """Path to sample conllu file."""
     return test_fixtures_path / "aug_sample_conllu.conllu"
 
+@pytest.fixture
+def vocabs(sample_conllu_file_path, augmented_sample_conllu_file_path) -> Dict[str, Dict[str, int]]:
+    """
+    Get vocabs from the sample and augmented file specified.
+    """
+    vocabs = {"TAG": [], "WORD": []}
+    for file in [sample_conllu_file_path, augmented_sample_conllu_file_path]:
+        with file.open() as in_file:
+            for line in in_file:
+                if line.strip():
+                    pieces = line.split()
+                    assert len(pieces) == 10
+                    vocabs['TAG'].append(pieces[3])
+                    vocabs['WORD'].append(pieces[1].lower())
+    for key, entry_list in vocabs.items():
+        entry_set = set(entry_list)
+        entry_index_map = {entry: i for i, entry in enumerate(entry_set)}
+        vocabs[key] = entry_index_map
+    return vocabs
+
+@pytest.fixture
+def basic_biaffine_transform(sample_conllu_file_path, augmented_sample_conllu_file_path):
+    files = [str(sample_conllu_file_path), str(augmented_sample_conllu_file_path)]
+    TAG, CHAR, ELMO, BERT = None, None, None, None
+    BOS = ""
+    TAG = Field('tags', bos=BOS)
+    ARC = Field('arcs', bos=BOS, use_vocab=False, fn=CoNLL.get_arcs)
+    REL = Field('rels', bos=BOS)
+    WORD = Field('words', pad=PAD, unk=UNK, bos=BOS, lower=True)
+    TEXT = RawField('texts')
+    transform = CoNLL(FORM=(WORD, TEXT, CHAR, ELMO, BERT), CPOS=TAG, HEAD=ARC, DEPREL=REL)
+    dataset = Dataset(transform, files)
+    TAG.build(dataset)
+    ARC.build(dataset)
+    REL.build(dataset)
+    WORD.build(dataset)
+    return transform
--- tests/test_data_load.py
+++ tests/test_data_load.py
@@ -2,18 +2,41 @@ from pathlib import Path
 from supar.utils.transform import Batch
 from supar.models.dep.biaffine.transform import CoNLL
 from supar.utils.data import Dataset, Sampler
+from supar.models.dep.biaffine.transform import CoNLL, CoNLLSentence
+from supar.utils.transform import Transform
 
 
-def test_basic_dataset_construction(sample_conllu_file_path: Path):
+def test_basic_dataset_construction(
+    sample_conllu_file_path: Path, basic_biaffine_transform: Transform
+):
     """Test that the dataset object can be instantiated using biaffine dependency parsing as an example."""
-    dataset = Dataset(transform=CoNLL(), data=str(sample_conllu_file_path))
+
+    dataset = Dataset(
+        transform=basic_biaffine_transform, data=str(sample_conllu_file_path)
+    )
     assert isinstance(dataset, Dataset)
-    assert len(dataset) == 1
+    assert len(dataset) == 2
+    dataset.build(batch_size=2)
 
 
-def test_dataset_with_augmentation(sample_conllu_file_path: Path, augmented_sample_conllu_file_path: Path):
+def test_dataset_with_augmentation(
+    sample_conllu_file_path: Path,
+    augmented_sample_conllu_file_path: Path,
+    basic_biaffine_transform: Transform,
+):
     """Test loading data with augmentation using low quality data."""
-    dataset = Dataset(transform=CoNLL(), data=str(sample_conllu_file_path), 
-                      data_for_augmentation=str(augmented_sample_conllu_file_path))
+    dataset = Dataset(
+        transform=basic_biaffine_transform,
+        data=str(sample_conllu_file_path),
+        data_for_augmentation=str(augmented_sample_conllu_file_path),
+    )
     assert isinstance(dataset, Dataset)
-    assert len(dataset) == 5
+    assert len(dataset) == 6
+    res = dataset.build(batch_size=2)
+    assert len(res.sentences) == 2
+    assert len(res.aug_sentences) == 4
+    for sentence in res.sentences:
+        assert isinstance(sentence, CoNLLSentence)
+    for sentence in res.aug_sentences:
+        assert isinstance(sentence, CoNLLSentence)
+
