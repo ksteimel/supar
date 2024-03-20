c9564b1b960c262b0f6ef76ace0cf9b6aab1e225
ksteimel <ksteimel@iu.edu>
2024-03-19 22:02:32 -0400

    Revert change that was causing failing unit tests This statement was needed because strings that do not exist as paths are interpreted as plain text to feed into the parser.

--- supar/models/dep/biaffine/transform.py
+++ supar/models/dep/biaffine/transform.py
@@ -400,9 +400,7 @@ class CoNLL(Transform):
         if lang is not None:
             tokenizer = Tokenizer(lang)
         with tempfile.TemporaryDirectory() as tdir:
-            if isinstance(data, str):
-                if not os.path.exists(data):
-                    raise RuntimeError(f"File path {data} does not exist.")
+            if isinstance(data, str) and os.path.exists(data):
                 f = open(data)
                 if data.endswith('.txt'):
                     lines = (i
