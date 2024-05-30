e951c229666c9fe9c365347cc9d0920bdd7c2494
ksteimel <ksteimel@iu.edu>
2024-05-29 23:07:37 -0400

    adding difficulty function for aug vs non-aug

--- tests/test_difficulty_functions.py
+++ tests/test_difficulty_functions.py
@@ -1,4 +1,4 @@
-from supar.utils.difficulty_functions import length, length_with_aug
+from supar.utils.difficulty_functions import length, length_with_aug, aug
 
 
 def test_length_difficulty_function(example_dataset):
@@ -26,5 +26,24 @@ def test_length_with_aug_data_difficulty_function(example_dataset):
         if sentence.is_aug:
             difficulty_offset = target_offset
         assert length_with_aug(sentence, aug_difficulty_offset = difficulty_offset) == len(sentence) + difficulty_offset
+
+
+def test_aug_difficulty_function(example_dataset):
+    """
+    Test that length with additional offset for augmentation works as expected.
+    """
+    # test default offset
+    for sentence in example_dataset:
+        difficulty_offset = 1
+        if sentence.is_aug:
+            difficulty_offset = 100
+        assert aug(sentence) == difficulty_offset
+    # test user specified offset
+    target_offset = 20
+    for sentence in example_dataset:
+        difficulty_offset = 1
+        if sentence.is_aug:
+            difficulty_offset = target_offset
+        assert aug(sentence, aug_difficulty_offset = difficulty_offset) == difficulty_offset
 
 
