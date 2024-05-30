1cc86be8c4422fb27911e2cae88655c6b28b3225
ksteimel <ksteimel@iu.edu>
2024-05-29 23:10:26 -0400

    adding difficulty function for aug vs non aug

--- supar/utils/difficulty_functions.py
+++ supar/utils/difficulty_functions.py
@@ -1,14 +1,14 @@
 from typing import List, Union
 
 
-def length(sentence, **kwargs) -> List[float]:
+def length(sentence, **kwargs) -> float:
     """
     A simple length based difficulty function.
     """
     return len(sentence)
 
 
-def length_with_aug(sentence, aug_difficulty_offset: Union[int, float] = 100):
+def length_with_aug(sentence, aug_difficulty_offset: Union[int, float] = 100, **kwargs):
     """
     A simple length based difficulty function where augmented sentences get an additional difficulty penalty.
     """
@@ -16,4 +16,13 @@ def length_with_aug(sentence, aug_difficulty_offset: Union[int, float] = 100):
         return len(sentence) + aug_difficulty_offset
     else:
         return len(sentence)
+
+def aug(sentence, aug_difficulty_offset: Union[int, float] = 100, **kwargs):
+    """
+    Difficulty function that only considers whether the sentence provided is augmented or not.
+    """
+    if sentence.is_aug:
+        return aug_difficulty_offset
+    else:
+        return 1
 
