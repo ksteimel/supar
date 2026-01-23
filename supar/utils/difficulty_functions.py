ee1e53ee18d563374fa62b871e87b7c436e0c2bf
ksteimel <ksteimel@iu.edu>
2026-01-23 12:56:03 -0500

    feat: aug offset is loaded from environment variables

--- supar/utils/difficulty_functions.py
+++ supar/utils/difficulty_functions.py
@@ -1,5 +1,7 @@
 from typing import List, Union
+import os
 
+default_aug_difficulty_offset = int(os.getenv("DEFAULT_AUG_DIFFICULTY_OFFSET", 100))
 
 def length(sentence, **kwargs) -> float:
     """
@@ -9,22 +11,25 @@ def length(sentence, **kwargs) -> float:
     return len(sentence)
 
 
-def length_with_aug(sentence, aug_difficulty_offset: Union[int, float] = 100, **kwargs):
+def length_with_aug(sentence, aug_difficulty_offset: Union[int, float] = default_aug_difficulty_offset, **kwargs):
     """
     A simple length based difficulty function where augmented sentences get an additional difficulty penalty.
     """
     print("using leng with aug difficulty function")
     print(f"{sentence.is_aug=}")
+    print(f"{aug_difficulty_offset=}")
     if sentence.is_aug:
         return len(sentence) + aug_difficulty_offset
     else:
         return len(sentence)
 
-def aug(sentence, aug_difficulty_offset: Union[int, float] = 100, **kwargs):
+def aug(sentence, aug_difficulty_offset: Union[int, float] = default_aug_difficulty_offset, **kwargs):
     """
     Difficulty function that only considers whether the sentence provided is augmented or not.
     """
     print("using aug difficulty function")
+    print(f"{sentence.is_aug=}")
+    print(f"{aug_difficulty_offset=}")
     if sentence.is_aug:
         return aug_difficulty_offset
     else:
